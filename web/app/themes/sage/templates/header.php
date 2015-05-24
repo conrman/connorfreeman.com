25a4afa5ace61a9d60c8680bcdf7d87a73072106
Connor Freeman <connormfreeman@gmail.com>
2015-05-24 14:19:15 -0500

    updated composer.json with new plugins

--- web/app/themes/sage/templates/header.php
+++ web/app/themes/sage/templates/header.php
@@ -1,10 +1,12 @@
-<header class="banner" role="banner">
-	<div class="container">
-		<a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-		<nav role="navigation">
+<header role="banner">
+	<div class="container-fluid">
+		<nav class="navbar" role="navigation">
+			<div class="nav-header">
+				<a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+			</div>
 			<?php
 			if (has_nav_menu('primary_navigation')) :
-				wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+				wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav']);
 			endif;
 			?>
 		</nav>
